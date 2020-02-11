3d1f462b07f3525ed85aa50bc4445780cb16a548
Simon Rüegg <simon.ruegg@vshn.ch>
2020-02-11 14:23:56 +0100

    Only update status if GitRepo was alredy created
    Otherwise there might be timing issues, where the object is not yet
    available if immediately queried for.
    
    Signed-off-by: Simon Rüegg <simon.ruegg@vshn.ch>

--- pkg/helpers/crd.go
+++ pkg/helpers/crd.go
@@ -15,14 +15,14 @@ import (
 )
 
 // CreateGitRepo will create the gitRepo object if it doesn't already exist. If the owner object itself is a tenant tenantRef can be set nil.
-func CreateGitRepo(obj metav1.Object, gvk schema.GroupVersionKind, template *synv1alpha1.GitRepoTemplate, client client.Client, tenantRef corev1.LocalObjectReference) error {
+func CreateGitRepo(obj metav1.Object, gvk schema.GroupVersionKind, template *synv1alpha1.GitRepoTemplate, client client.Client, tenantRef corev1.LocalObjectReference) (bool, error) {
 
 	if template == nil {
-		return fmt.Errorf("gitRepo template is empty")
+		return false, fmt.Errorf("gitRepo template is empty")
 	}
 
 	if tenantRef.Name == "" {
-		return fmt.Errorf("the tenant name is empty")
+		return false, fmt.Errorf("the tenant name is empty")
 	}
 
 	repo := &synv1alpha1.GitRepo{
@@ -50,14 +50,14 @@ func CreateGitRepo(obj metav1.Object, gvk schema.GroupVersionKind, template *syn
 
 		err = client.Get(context.TODO(), namespacedName, existingRepo)
 		if err != nil {
-			return fmt.Errorf("could not update existing repo: %v", err)
+			return false, fmt.Errorf("could not update existing repo: %v", err)
 		}
 
 		existingRepo.Spec = repo.Spec
 
-		return client.Update(context.TODO(), existingRepo)
+		return false, client.Update(context.TODO(), existingRepo)
 	} else if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return true, nil
 }
