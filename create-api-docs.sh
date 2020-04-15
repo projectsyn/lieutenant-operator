2b22fbe67e294e9acd922ffa820ef4123682bc4d
Simon Beck <simon.beck@vshn.ch>
2020-04-15 10:58:59 +0200

    Fix include

--- create-api-docs.sh
+++ create-api-docs.sh
@@ -15,7 +15,7 @@ ln -s "$(pwd)" "$GOPATH/src/github.com/projectsyn/lieutenant-operator"
 
 cd "$GOPATH/src/github.com/projectsyn/lieutenant-operator"
 
-go run github.com/ahmetb/gen-crd-api-reference-docs -config gen-api.json -api-dir ./pkg/apis -out-file docs/modules/ROOT/pages/crds.html
+go run github.com/ahmetb/gen-crd-api-reference-docs -config gen-api.json -api-dir ./pkg/apis -out-file docs/modules/ROOT/partials/crds.html
 
 # the permissions are weird for some reason after go run...
 chmod -R 770 tempgopath
