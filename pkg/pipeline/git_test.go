5ccb0cfcba616f3e5e3016e3c0e5968782127573
Simon Beck <simon.beck@earthnet.ch>
2020-10-27 10:28:40 +0100

    Align git repo unit tests with the rest

--- pkg/pipeline/git_test.go
+++ pkg/pipeline/git_test.go
@@ -9,172 +9,163 @@ import (
 	synv1alpha1 "github.com/projectsyn/lieutenant-operator/pkg/apis/syn/v1alpha1"
 	"github.com/projectsyn/lieutenant-operator/pkg/git/manager"
 	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func TestCreateOrUpdateGitRepo(t *testing.T) {
-	type args struct {
-		obj       *synv1alpha1.Cluster
-		template  *synv1alpha1.GitRepoTemplate
-		tenantRef v1.LocalObjectReference
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "create git repo",
-			args: args{
-				obj: &synv1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test",
-						Namespace: "test",
-					},
-				},
-				template: &synv1alpha1.GitRepoTemplate{
-					APISecretRef: v1.SecretReference{Name: "testSecret"},
-					DeployKeys:   nil,
-					Path:         "testPath",
-					RepoName:     "testRepo",
-				},
-				tenantRef: v1.LocalObjectReference{
-					Name: "testTenant",
-				},
-			},
-		},
-		{
-			name: "empty template",
-			args: args{
-				obj: &synv1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test",
-						Namespace: "test",
-					},
-				},
-				template: nil,
-				tenantRef: v1.LocalObjectReference{
-					Name: "testTenant",
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
+type gitRepoArgs struct {
+	repo        *synv1alpha1.GitRepo
+	cluster     *synv1alpha1.Cluster
+	template    *synv1alpha1.GitRepoTemplate
+	tenantRef   corev1.LocalObjectReference
+	templateObj PipelineObject
+	data        *ExecutionContext
+}
 
+var createOrUpdateGitRepoCases = map[string]struct {
+	args    gitRepoArgs
+	wantErr bool
+}{
+	"create git repo": {
+		args: gitRepoArgs{
+			cluster: &synv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+				},
+			},
+			template: &synv1alpha1.GitRepoTemplate{
+				APISecretRef: corev1.SecretReference{Name: "testSecret"},
+				DeployKeys:   nil,
+				Path:         "testPath",
+				RepoName:     "testRepo",
+			},
+			tenantRef: corev1.LocalObjectReference{
+				Name: "testTenant",
+			},
+		},
+	},
+	"empty template": {
+		args: gitRepoArgs{
+			cluster: &synv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+				},
+			},
+			template: nil,
+			tenantRef: corev1.LocalObjectReference{
+				Name: "testTenant",
+			},
+		},
+	},
+}
+
+func TestCreateOrUpdateGitRepo(t *testing.T) {
+	for name, tt := range createOrUpdateGitRepoCases {
 		objs := []runtime.Object{
 			&synv1alpha1.GitRepo{},
 		}
 
 		cl, _ := testSetupClient(objs)
 
-		tt.args.obj.Spec.GitRepoTemplate = tt.args.template
-		tt.args.obj.Spec.TenantRef = tt.args.tenantRef
+		tt.args.cluster.Spec.GitRepoTemplate = tt.args.template
+		tt.args.cluster.Spec.TenantRef = tt.args.tenantRef
 
-		t.Run(tt.name, func(t *testing.T) {
-			if res := createGitRepo(tt.args.obj, &ExecutionContext{Client: cl}); (res.Err != nil) != tt.wantErr {
+		t.Run(name, func(t *testing.T) {
+			if res := createGitRepo(tt.args.cluster, &ExecutionContext{Client: cl}); (res.Err != nil) != tt.wantErr {
 				t.Errorf("CreateGitRepo() error = %v, wantErr %v", res.Err, tt.wantErr)
 			}
+
+			if tt.args.template != nil {
+				namespacedName := types.NamespacedName{
+					Name:      tt.args.cluster.GetName(),
+					Namespace: tt.args.cluster.GetNamespace(),
+				}
+
+				checkRepo := &synv1alpha1.GitRepo{}
+				assert.NoError(t, cl.Get(context.TODO(), namespacedName, checkRepo))
+				assert.Equal(t, tt.args.template, &checkRepo.Spec.GitRepoTemplate)
+			}
 		})
-
-		if tt.args.template == nil {
-			continue
-		}
-
-		namespacedName := types.NamespacedName{
-			Name:      tt.args.obj.GetName(),
-			Namespace: tt.args.obj.GetNamespace(),
-		}
-
-		checkRepo := &synv1alpha1.GitRepo{}
-		assert.NoError(t, cl.Get(context.TODO(), namespacedName, checkRepo))
-		assert.Equal(t, tt.args.template, &checkRepo.Spec.GitRepoTemplate)
-
 	}
 }
 
+var fetchGitRepoTemplateCases = map[string]struct {
+	args    gitRepoArgs
+	wantErr bool
+}{
+	"fetch tenant changes": {
+		args: gitRepoArgs{
+			data: &ExecutionContext{},
+			repo: &synv1alpha1.GitRepo{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+			templateObj: &synv1alpha1.Tenant{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: synv1alpha1.TenantSpec{
+					GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
+						RepoName: "Test Repo",
+						RepoType: synv1alpha1.AutoRepoType,
+						Path:     "test",
+					},
+				},
+			},
+		},
+	},
+	"fetch cluster changes": {
+		args: gitRepoArgs{
+			data: &ExecutionContext{},
+			repo: &synv1alpha1.GitRepo{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+			templateObj: &synv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: synv1alpha1.ClusterSpec{
+					GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
+						RepoName: "Test Repo",
+						RepoType: synv1alpha1.AutoRepoType,
+						Path:     "test",
+					},
+				},
+			},
+		},
+	},
+}
+
 func Test_fetchGitRepoTemplate(t *testing.T) {
-	type args struct {
-		obj         *synv1alpha1.GitRepo
-		templateObj PipelineObject
-		data        *ExecutionContext
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "fetch tenant changes",
-			args: args{
-				data: &ExecutionContext{},
-				obj: &synv1alpha1.GitRepo{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-				},
-				templateObj: &synv1alpha1.Tenant{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: synv1alpha1.TenantSpec{
-						GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
-							RepoName: "Test Repo",
-							RepoType: synv1alpha1.AutoRepoType,
-							Path:     "test",
-						},
-					},
-				},
-			},
-		},
-		{
-			name: "fetch cluster changes",
-			args: args{
-				data: &ExecutionContext{},
-				obj: &synv1alpha1.GitRepo{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-				},
-				templateObj: &synv1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: synv1alpha1.ClusterSpec{
-						GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
-							RepoName: "Test Repo",
-							RepoType: synv1alpha1.AutoRepoType,
-							Path:     "test",
-						},
-					},
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range fetchGitRepoTemplateCases {
+		t.Run(name, func(t *testing.T) {
 			rtObj := tt.args.templateObj.(runtime.Object)
 
 			tt.args.data.Client, _ = testSetupClient([]runtime.Object{
-				tt.args.obj,
+				tt.args.repo,
 				&synv1alpha1.Tenant{},
 				&synv1alpha1.Cluster{},
 				rtObj,
 			})
 
-			if err := fetchGitRepoTemplate(tt.args.obj, tt.args.data); (err != nil) != tt.wantErr {
+			if err := fetchGitRepoTemplate(tt.args.repo, tt.args.data); (err != nil) != tt.wantErr {
 				t.Errorf("fetchGitRepoTemplate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 
-			assert.Equal(t, tt.args.templateObj.GetGitTemplate(), &tt.args.obj.Spec.GitRepoTemplate)
+			assert.Equal(t, tt.args.templateObj.GetGitTemplate(), &tt.args.repo.Spec.GitRepoTemplate)
 			tt.args.templateObj.GetGitTemplate().RepoName = "another test"
 			rtObj = tt.args.templateObj.(runtime.Object)
 			assert.NoError(t, tt.args.data.Client.Update(context.TODO(), rtObj))
-			assert.NoError(t, fetchGitRepoTemplate(tt.args.obj, tt.args.data))
-			assert.Equal(t, tt.args.templateObj.GetGitTemplate(), &tt.args.obj.Spec.GitRepoTemplate)
+			assert.NoError(t, fetchGitRepoTemplate(tt.args.repo, tt.args.data))
+			assert.Equal(t, tt.args.templateObj.GetGitTemplate(), &tt.args.repo.Spec.GitRepoTemplate)
 		})
 	}
 }
@@ -197,72 +188,61 @@ func (r *repoMock) Connect() error             { return nil }
 func (r *repoMock) Remove() error              { return nil }
 func (r *repoMock) CommitTemplateFiles() error { return nil }
 
+var repoExistsCases = map[string]struct {
+	args manager.Repo
+	want bool
+}{
+	"repo exists": {
+		want: true,
+		args: &repoMock{},
+	},
+	"repo doesn't exist": {
+		want: false,
+		args: &repoMock{
+			failRead: true,
+		},
+	}}
+
 func Test_repoExists(t *testing.T) {
-	type args struct {
-		repo manager.Repo
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "repo exists",
-			want: true,
-			args: args{
-				repo: &repoMock{},
-			},
-		},
-		{
-			name: "repo doesn't exist",
-			want: false,
-			args: args{
-				repo: &repoMock{
-					failRead: true,
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := repoExists(tt.args.repo); got != tt.want {
+	for name, tt := range repoExistsCases {
+		t.Run(name, func(t *testing.T) {
+			if got := repoExists(tt.args); got != tt.want {
 				t.Errorf("repoExists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
+type handleRepoErrorArgs struct {
+	err      error
+	instance *synv1alpha1.GitRepo
+	repo     manager.Repo
+	fail     bool
+}
+
+var handleRepoErrorCases = map[string]struct {
+	args handleRepoErrorArgs
+}{
+	"add error": {
+		args: handleRepoErrorArgs{
+			err:      fmt.Errorf("lol nope"),
+			instance: &synv1alpha1.GitRepo{},
+			repo:     &repoMock{},
+		},
+	},
+	"add error failure": {
+		args: handleRepoErrorArgs{
+			err:      fmt.Errorf("lol nope"),
+			instance: &synv1alpha1.GitRepo{},
+			repo:     &repoMock{},
+			fail:     true,
+		},
+	},
+}
+
 func Test_handleRepoError(t *testing.T) {
-	type args struct {
-		err      error
-		instance *synv1alpha1.GitRepo
-		repo     manager.Repo
-		fail     bool
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "add error",
-			args: args{
-				err:      fmt.Errorf("lol nope"),
-				instance: &synv1alpha1.GitRepo{},
-				repo:     &repoMock{},
-			},
-		},
-		{
-			name: "add error failure",
-			args: args{
-				err:      fmt.Errorf("lol nope"),
-				instance: &synv1alpha1.GitRepo{},
-				repo:     &repoMock{},
-				fail:     true,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range handleRepoErrorCases {
+		t.Run(name, func(t *testing.T) {
 
 			var client client.Client
 
@@ -285,65 +265,58 @@ func Test_handleRepoError(t *testing.T) {
 	}
 }
 
+var setGitRepoURLAndHostKeysCases = map[string]struct {
+	args    gitRepoArgs
+	wantErr bool
+}{
+	"set url and keys": {
+		wantErr: false,
+		args: gitRepoArgs{
+			cluster: &synv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+			data: &ExecutionContext{},
+			repo: &synv1alpha1.GitRepo{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Status: synv1alpha1.GitRepoStatus{
+					URL:      "someURL",
+					HostKeys: "someKeys",
+				},
+			},
+		},
+	},
+	"set url and keys not found": {
+		wantErr: false,
+		args: gitRepoArgs{
+			cluster: &synv1alpha1.Cluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "invalid",
+				},
+			},
+			data: &ExecutionContext{},
+			repo: &synv1alpha1.GitRepo{},
+		},
+	},
+}
+
 func Test_setGitRepoURLAndHostKeys(t *testing.T) {
-	type args struct {
-		obj     *synv1alpha1.Cluster
-		gitRepo *synv1alpha1.GitRepo
-		data    *ExecutionContext
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name:    "set url and keys",
-			wantErr: false,
-			args: args{
-				obj: &synv1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-				},
-				data: &ExecutionContext{},
-				gitRepo: &synv1alpha1.GitRepo{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Status: synv1alpha1.GitRepoStatus{
-						URL:      "someURL",
-						HostKeys: "someKeys",
-					},
-				},
-			},
-		},
-		{
-			name:    "set url and keys not found",
-			wantErr: false,
-			args: args{
-				obj: &synv1alpha1.Cluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "invalid",
-					},
-				},
-				data:    &ExecutionContext{},
-				gitRepo: &synv1alpha1.GitRepo{},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for name, tt := range setGitRepoURLAndHostKeysCases {
+		t.Run(name, func(t *testing.T) {
 			tt.args.data.Client, _ = testSetupClient([]runtime.Object{
-				tt.args.obj,
-				tt.args.gitRepo,
+				tt.args.cluster,
+				tt.args.repo,
 			})
 
-			if got := setGitRepoURLAndHostKeys(tt.args.obj, tt.args.data); (got.Err != nil) != tt.wantErr {
+			if got := setGitRepoURLAndHostKeys(tt.args.cluster, tt.args.data); (got.Err != nil) != tt.wantErr {
 				t.Errorf("setGitRepoURLAndHostKeys() = had error: %v", got.Err)
 			}
 
-			assert.Equal(t, tt.args.gitRepo.Status.URL, tt.args.obj.Spec.GitRepoURL)
-			assert.Equal(t, tt.args.gitRepo.Status.HostKeys, tt.args.obj.Spec.GitHostKeys)
+			assert.Equal(t, tt.args.repo.Status.URL, tt.args.cluster.Spec.GitRepoURL)
+			assert.Equal(t, tt.args.repo.Status.HostKeys, tt.args.cluster.Spec.GitHostKeys)
 		})
 	}
 }
