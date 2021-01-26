9eb6e6f28c3232bf8d99ca8b7e169ae6be21116b
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2021-01-26 14:11:48 +0100

    Run generators

--- pkg/apis/syn/v1alpha1/tenant_template_types.go
+++ pkg/apis/syn/v1alpha1/tenant_template_types.go
@@ -8,7 +8,7 @@ import (
 
 // TenantTemplate is the Schema for the tenant templates API
 // +kubebuilder:subresource:status
-// +kubebuilder:resource:path=tenants,scope=Namespaced
+// +kubebuilder:resource:path=tenanttemplates,scope=Namespaced
 // +kubebuilder:printcolumn:name="Display Name",type="string",JSONPath=".spec.displayName"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type TenantTemplate struct {
@@ -20,7 +20,7 @@ type TenantTemplate struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// TenantTemplateList contains a list of Tenant
+// TenantTemplateList contains a list of TenantTemplate
 type TenantTemplateList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
