fab86aa94b9d1ddfca5148dd05947f8970d8b422
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2021-01-26 15:29:34 +0100

    Test that the tenant template gets applied

---
+++ pkg/apis/syn/v1alpha1/tenant_types_test.go
@@ -0,0 +1,67 @@
+package v1alpha1_test
+
+import (
+	"testing"
+
+	synv1alpha1 "github.com/projectsyn/lieutenant-operator/pkg/apis/syn/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+var testTenantTemplateSpec = synv1alpha1.TenantSpec{
+	DisplayName:           "Tenant Name",
+	GitRepoURL:            "ssh://git@example.com/my-tenant.git",
+	GitRepoRevision:       "my-tenant-branch",
+	GlobalGitRepoURL:      "ssh://git@example.com/my-config.git",
+	GlobalGitRepoRevision: "my-config-branch",
+	GitRepoTemplate:       &synv1alpha1.GitRepoTemplate{},
+	DeletionPolicy:        synv1alpha1.DeletePolicy,
+	ClusterTemplate: &synv1alpha1.ClusterSpec{
+		DeletionPolicy: synv1alpha1.ArchivePolicy,
+	},
+}
+
+var tenantTemplateCases = map[string]struct {
+	template *synv1alpha1.TenantTemplate
+	tenant   *synv1alpha1.Tenant
+	expected *synv1alpha1.Tenant
+}{
+	"template gets applied": {
+		template: &synv1alpha1.TenantTemplate{
+			Spec: testTenantTemplateSpec,
+		},
+		tenant: &synv1alpha1.Tenant{},
+		expected: &synv1alpha1.Tenant{
+			Spec: testTenantTemplateSpec,
+		},
+	},
+	"tenant values take precedence": {
+		template: &synv1alpha1.TenantTemplate{
+			Spec: synv1alpha1.TenantSpec{
+				DeletionPolicy: synv1alpha1.DeletePolicy,
+			},
+		},
+		tenant: &synv1alpha1.Tenant{
+			Spec: synv1alpha1.TenantSpec{
+				DisplayName:    "Tenant Name",
+				DeletionPolicy: synv1alpha1.ArchivePolicy,
+			},
+		},
+		expected: &synv1alpha1.Tenant{
+			Spec: synv1alpha1.TenantSpec{
+				DisplayName:    "Tenant Name",
+				DeletionPolicy: synv1alpha1.ArchivePolicy,
+			},
+		},
+	},
+}
+
+func TestTenant_ApplyTemplate(t *testing.T) {
+	for name, tt := range tenantTemplateCases {
+		t.Run(name, func(t *testing.T) {
+			actual := tt.tenant.DeepCopy()
+			err := actual.ApplyTemplate(tt.template)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
