217b111e8919bd8f33c58ff3852542d5f42c368a
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2021-02-04 15:56:10 +0100

    DRYup tenant reconcile tests

--- pkg/controller/tenant/tenant_reconcile_test.go
+++ pkg/controller/tenant/tenant_reconcile_test.go
@@ -2,15 +2,13 @@ package tenant
 
 import (
 	"context"
-	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/projectsyn/lieutenant-operator/pkg/apis"
 	synv1alpha1 "github.com/projectsyn/lieutenant-operator/pkg/apis/syn/v1alpha1"
 	"github.com/projectsyn/lieutenant-operator/pkg/pipeline"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -24,6 +22,24 @@ func testSetupClient(objs []runtime.Object) (client.Client, *runtime.Scheme) {
 	s := scheme.Scheme
 	s.AddKnownTypes(synv1alpha1.SchemeGroupVersion, objs...)
 	return fake.NewFakeClientWithScheme(s, objs...), s
+}
+
+func readObject(t *testing.T, c client.Client, ns types.NamespacedName, obj runtime.Object) {
+	err := c.Get(context.Background(), ns, obj)
+	require.NoError(t, err)
+}
+
+func reconcileTenant(t *testing.T, c client.Client, s *runtime.Scheme, name types.NamespacedName) reconcile.Result {
+	r := &ReconcileTenant{client: c, scheme: s}
+
+	req := reconcile.Request{
+		NamespacedName: name,
+	}
+
+	result, err := r.Reconcile(req)
+	require.NoError(t, err)
+
+	return result
 }
 
 func TestHandleNilGitRepoTemplate(t *testing.T) {
@@ -53,83 +69,46 @@ func TestHandleNilGitRepoTemplate(t *testing.T) {
 	_, err := r.Reconcile(req)
 	assert.NoError(t, err)
 	updatedTenant := &synv1alpha1.Tenant{}
-	err = cl.Get(context.TODO(), types.NamespacedName{Name: tenant.Name}, updatedTenant)
-	assert.NoError(t, err)
+	readObject(t, cl, req.NamespacedName, updatedTenant)
 	assert.Contains(t, updatedTenant.Spec.GitRepoTemplate.TemplateFiles, "common.yml")
 }
 
 func TestCreateGitRepo(t *testing.T) {
-	tests := []struct {
-		name      string
-		want      reconcile.Result
-		wantErr   bool
-		objName   string
-		namespace string
-	}{
-		{
-			name:      "Git repo object created",
-			want:      reconcile.Result{},
-			wantErr:   false,
-			objName:   "my-git-repo",
-			namespace: "tenant",
+	tenant := &synv1alpha1.Tenant{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-tenant",
+			Namespace: "my-namespace",
+		},
+		Spec: synv1alpha1.TenantSpec{
+			DisplayName: "My Tenant",
+			GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
+				RepoName: "repo-name",
+			},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
 
-			tenant := &synv1alpha1.Tenant{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      tt.objName,
-					Namespace: tt.namespace,
-				},
-				Spec: synv1alpha1.TenantSpec{
-					DisplayName: "Display Name",
-					GitRepoTemplate: &synv1alpha1.GitRepoTemplate{
-						RepoName: "repo-name",
-					},
-				},
-			}
-
-			objs := []runtime.Object{
-				tenant,
-				&synv1alpha1.GitRepo{},
-				&synv1alpha1.ClusterList{},
-			}
-
-			cl, s := testSetupClient(objs)
-
-			r := &ReconcileTenant{client: cl, scheme: s}
-
-			req := reconcile.Request{
-				NamespacedName: types.NamespacedName{
-					Name:      tt.objName,
-					Namespace: tt.namespace,
-				},
-			}
-
-			got, err := r.Reconcile(req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Reconcile() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Reconcile() got = %v, want %v", got, tt.want)
-			}
-
-			gitRepoNamespacedName := types.NamespacedName{
-				Name:      tt.objName,
-				Namespace: tt.namespace,
-			}
-
-			gitRepo := &synv1alpha1.GitRepo{}
-			err = cl.Get(context.TODO(), gitRepoNamespacedName, gitRepo)
-			assert.NoError(t, err)
-			assert.Equal(t, tenant.Spec.DisplayName, gitRepo.Spec.GitRepoTemplate.DisplayName)
-			fileContent, found := gitRepo.Spec.GitRepoTemplate.TemplateFiles[pipeline.CommonClassName+".yml"]
-			assert.True(t, found)
-			assert.Equal(t, "", fileContent)
-		})
+	objs := []runtime.Object{
+		tenant,
+		&synv1alpha1.GitRepo{},
+		&synv1alpha1.ClusterList{},
 	}
+
+	cl, s := testSetupClient(objs)
+
+	name := types.NamespacedName{
+		Name:      tenant.Name,
+		Namespace: tenant.Namespace,
+	}
+
+	reconcileTenant(t, cl, s, name)
+
+	gitRepo := &synv1alpha1.GitRepo{}
+	readObject(t, cl, name, gitRepo)
+
+	assert.Equal(t, tenant.Spec.DisplayName, gitRepo.Spec.GitRepoTemplate.DisplayName)
+	fileContent, found := gitRepo.Spec.GitRepoTemplate.TemplateFiles[pipeline.CommonClassName+".yml"]
+	assert.True(t, found)
+	assert.Equal(t, "", fileContent)
 }
 
 func TestCreateClusterClass(t *testing.T) {
@@ -173,20 +152,14 @@ func TestCreateClusterClass(t *testing.T) {
 
 	cl, s := testSetupClient(objs)
 
-	r := &ReconcileTenant{client: cl, scheme: s}
-
-	req := reconcile.Request{
-		NamespacedName: types.NamespacedName{
-			Name: tenant.Name,
-		},
+	name := types.NamespacedName{
+		Name: tenant.Name,
 	}
 
-	_, err := r.Reconcile(req)
-	require.NoError(t, err)
+	reconcileTenant(t, cl, s, name)
 
 	updatedTenant := &synv1alpha1.Tenant{}
-	err = cl.Get(context.Background(), req.NamespacedName, updatedTenant)
-	require.NoError(t, err)
+	readObject(t, cl, name, updatedTenant)
 
 	assert.Contains(t, updatedTenant.Spec.GitRepoTemplate.TemplateFiles[clusterA.Name+".yml"], tenant.Name)
 	assert.Contains(t, updatedTenant.Spec.GitRepoTemplate.TemplateFiles[clusterB.Name+".yml"], tenant.Name)
