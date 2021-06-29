3405cdcecedf0ee89eb653966b031fa9136735a8
Chris <github.account@chrigel.net>
2021-06-29 10:35:32 +0200

    Rename service account

--- tools.go
+++ tools.go
@@ -6,6 +6,6 @@ package tools
 
 import (
 	_ "github.com/ahmetb/gen-crd-api-reference-docs"
-	_ "sigs.k8s.io/kustomize"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+	_ "sigs.k8s.io/kustomize"
 )
