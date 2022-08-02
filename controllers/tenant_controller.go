b96a1b2878e2b053ff02019995fa4a2c8a2c0309
Simon Gerber <simon.gerber@vshn.ch>
2022-08-02 11:40:18 +0200

    Add kubebuilder annotation for managing ServiceAccounts

--- controllers/tenant_controller.go
+++ controllers/tenant_controller.go
@@ -26,6 +26,7 @@ type TenantReconciler struct {
 //+kubebuilder:rbac:groups=syn.tools,resources=tenants/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=syn.tools,resources=tenants/finalizers,verbs=update
 //+kubebuilder:rbac:groups=syn.tools,resources=tenanttemplates,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile The Controller will requeue the Request to be processed again if the returned error is non-nil or
 // Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
